e37f2f9e9ccfe74bf61bf0d6241d771ab4ed8922
jinesh42 <jain.jinesh220@gmail.com>
2025-01-17 00:14:15 +0530

    handling blank values in dbo_contains

--- bos-onboarding/sections/device_section.py
+++ bos-onboarding/sections/device_section.py
@@ -232,33 +232,37 @@ class DBOVirtualDeviceSection(DeviceSection):
     feeds = row[self._site_model_columns.CONNECTION_FEEDS]
     controls = row[self._site_model_columns.CONNECTION_CONTROLS]
 
-    b={}
-    for i in self._site_model_sheets.LOCATIONS:
-      b[i['dbo.entity_name']]=i['dbo.id']
-    
-    
-    c={}
-    #Creating seperate dictionary for all the asset.guid values from asset tab of excel
-    for i in self._site_model_sheets.ASSETS:
-      c[i['entity_instance_name']]=i['udmi.physical_tag.asset.guid']
-    
-    contains_value=[]
-    #Handling multiple values from dbo.connections.contains from asset tab of excel 
     conn_contains = row[self._site_model_columns.CONNECTION_CONTAINS].split(',')
-    for j in conn_contains:
-      j=j.strip()
-      # Checking value present in entity name from dbo.id dictionary
-      if j in b :
+    #Handling values 
+    if not ''.join(conn_contains).isspace() and len(feeds) > 0:
+    # contains
+      b={}
+      for i in self._site_model_sheets.LOCATIONS:
+        b[i['dbo.entity_name']]=i['dbo.id']
+    
+    
+      c={}
+      #Creating seperate dictionary for all the asset.guid values from asset tab of excel
+      for i in self._site_model_sheets.ASSETS:
+        c[i['entity_instance_name']]=i['udmi.physical_tag.asset.guid']
+    
+      contains_value=[]
+      #Handling multiple values from dbo.connections.contains from asset tab of excel 
+    
+      for j in conn_contains:
+        j=j.strip()
+        # Checking value present in entity name from dbo.id dictionary
+        if j in b :
         
-        contains_value.append(b[j])
+          contains_value.append(b[j])
 
-      else:
-        contains_value=j
+        else:
+          contains_value=j
 
     
-    contains_val=','.join(contains_value)
+      contains_val=','.join(contains_value)
 
-    device.populate_connections(contains_val, "CONTAINS")
+      device.populate_connections(contains_val, "CONTAINS")
 
     if not feeds.isspace() and len(feeds) > 0:
       feeds = self._get_devices_from_string(feeds)
